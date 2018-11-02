d1518e1209e36568ecc2d12ec55cf8ea6749d00a
Albert Avery <albertavery@swuser-129-8-216-197.swlan.csufresno.edu>
2018-11-01 17:18:19 -0700

    added some tabs and a different graph

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,9 @@
 import {MatButtonModule, MatCheckboxModule, MatSidenavModule} from '@angular/material';
+import {MatTabsModule, MatTab} from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule],
 })
 export class MaterialModule { }
