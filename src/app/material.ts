d0a01cd26d7ad2366a86e8449e1f47a846e131d3
Albert Avery <albertavery@swuser-129-8-216-225.swlan.csufresno.edu>
2018-11-15 16:57:19 -0800

    toolbar and icons

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,12 @@
 import {MatButtonModule, MatCheckboxModule, MatSidenavModule} from '@angular/material';
 import {MatTabsModule, MatTab} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule, MatToolbarModule, MatIconModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatTabsModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule { }
